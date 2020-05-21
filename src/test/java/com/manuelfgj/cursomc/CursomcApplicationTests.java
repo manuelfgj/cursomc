2670782771d53944f9e28bb194dbc87aed989e28
Manuel Gomes <manuel.fgj@gmail.com>
2020-05-21 19:41:06 -0300

    Projeto criado

---
+++ src/test/java/com/manuelfgj/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelfgj.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
