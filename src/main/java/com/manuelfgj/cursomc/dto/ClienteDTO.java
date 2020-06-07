ff55525cf0cb3809abdaf80d5028e263964b3080
Manuel Gomes <manuel.fgj@gmail.com>
2020-06-07 11:19:36 -0300

    Validacao customizada: email não repetido na atualizacao de Cliente

--- src/main/java/com/manuelfgj/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/manuelfgj/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.manuelfgj.cursomc.domain.Cliente;
+import com.manuelfgj.cursomc.services.validation.ClientUpdate;
 
+@ClientUpdate
 public class ClienteDTO  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
